f0382af65649935dae8d255b6e12a48905d6db46
Juergen Baltres <juergen.baltres@gmail.com>
2019-08-28 21:37:15 +0200

    try first canvas commands

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let formInput1 = document.querySelector(".first");
+/*let formInput1 = document.querySelector(".first");
 let formOutput1 = document.querySelector(".output1");
 let formInput2 = document.querySelector(".second");
 let formOutput2 = document.querySelector(".output2");
@@ -24,3 +24,13 @@ formInput1.addEventListener("keyup", generateText);
 formInput2.addEventListener("keyup", generateText2);
 formInput3.addEventListener("keyup", generateText3);
 formInput4.addEventListener("keyup", generateText4);
+*/
+
+let canvas = document.querySelector(".canvas");
+let ctx = canvas.getContext("2d");
+
+const image = document.querySelector(".little-image");
+image.src =
+  "http://www.team-bhp.com/forum/attachments/travelogues/1079291d1367318179-bolero-4wd-takes-us-sandakphu-3rd-time-dsc_1018.jpg";
+
+ctx.drawImage(image, 0, 0, 100, 100);
